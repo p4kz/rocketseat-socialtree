66b88e8f6017870dcc4eccd339532a03e127072c
p4kz <wolpak@hotmail.com>
2022-02-23 06:30:27 -0300

    fix java/html and css and color correct.

--- script/main.js
+++ script/main.js
@@ -1,24 +1,7 @@
-let list = document.querySelectorAll('li, .mainContainer, img');
-let bg = document.querySelector('.mainContainer');
-list.forEach(elements =>{
-    elements.addEventListener('mouseenter', function
-    (event){
-        let color = event.target.getAttribute
-        ('data-color');
-        bg.style.backgroundColor = color;
-    })
-
-    elements.addEventListener('mouseleave', function
-    (event){
-        
-        bg.style.backgroundColor = '#fff';
-    })
-})
-
 VanillaTilt.init(document.querySelectorAll
     ("li, img, .socialmedia, .mainContainer"),{
-    max: 25,
-    speed: 400,
+    max: 20,
+    speed: 100,
     glare: false,
     "max-glare": 0,
     });
